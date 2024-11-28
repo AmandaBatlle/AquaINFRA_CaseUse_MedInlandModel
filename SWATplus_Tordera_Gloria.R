365d923ea831e12d88f95dd469a646b891a64fd8
AmandaBatlle <65679557+AmandaBatlle@users.noreply.github.com>
2024-11-28 08:02:56 +0100

    Update SWATplus_Tordera_Gloria.R

--- SWATplus_Tordera_Gloria.R
+++ SWATplus_Tordera_Gloria.R
@@ -13,15 +13,16 @@ library(dplyr) # To convert data for export
 
 # Funtion to run SWAt+ and process output
 
-run_swat_process <- function (TxtInOut) {
+run_swat_process <- function (TxtInOut, par_comb) {
   #Run SWAT+ simulation
   q_sim_plus <- run_swatplus(project_path = TxtInOut,
-                             output = define_output(file = 'channel_sd_mon',
+                             output = define_output(file = 'channel_sd_day',
                                                     variable = 'flo_out',
                                                     unit = 1),
                              start_date= 20160101,
                              end_date=20201231,
-                             start_date_print = 20190601)
+                             start_date_print = 20190601,
+                            parameter=par_comb)
   
   # Process the output: rename the column to Sim_Flow
   q_plus <- q_sim_plus$simulation$flo_out %>%
@@ -35,9 +36,17 @@ run_swat_process <- function (TxtInOut) {
 # ‘chmod +x rev688_64rel_linux’ 
 
 
-# Example of how to call the function
-path_TxtInOut <- "C:(PATH TO DATA DIRECTORY)/Scenario_Gloria_windows"
-q_plus_result <- run_swat_process(path_TxtInOut)
+# Function call
+path_TxtInOut <- "C:(PATH TO DATA DIRECTORY)/Scenario_Gloria_linux"
+# Parameter change
+par_cal <- c("cn2.hru | change=absval" = -15.238,
+              "esco.hru | change=absval" = 0.805,
+              "canmx.hru | change=absval" = 81.537,
+              "perco.hru | change=absval" = 0.892,
+              "cn3_swf.hru | change=absval" = 0.819
+              )
+
+q_plus_result <- run_swat_process(path_TxtInOut, par_cal)
 
 # View the result
 head(q_plus_result)
