4eceb7dccfd575a6a38cd018a74aea2f75cb42df
AmandaBatlle <65679557+AmandaBatlle@users.noreply.github.com>
2025-04-18 11:21:59 +0300

    Update swat_mitgcm_connection.R
    Updated to take as input an SQL database.

--- src/swat_mitgcm_connection.R
+++ src/swat_mitgcm_connection.R
@@ -4,58 +4,58 @@
 # AquaINFRA Mediterranean Case Use: 
 # Step 2 Mediterranean inland to marine model connection: 
 
-# Tool Name: SWAT+ output to MITGCM input connection tool.
+# Tool Name: SWAT+ output to MITgcm input connection tool.
 #
 # Description: 
-#   This code converts SWAT output of water quantity (Flow out and Temperature) in csv format to date input for MITGCM in txt format. 
+#   This code converts SWAT output of water quantity (Flow out and Temperature) in SQL format to data input for MITgcm in txt format. 
 #
 # Inputs: 
-#   id: flow_file
-#   Title: Daily river flow out
-#   Data Type: string
-#   Description: csv file containing daily flow out values (units: m3/s). 
-# 
-#   id: temp_file
-#   Title: Daily river water temperature
-#   Data Type: string
-#   Description: csv file containing daily water temperature values (units: Degrees Celsius).
-#
+#   "swat_output_file": 
+#             "title": "SWAT+ Variable Outputs Database",
+#             "description": "SQLite database (thread_1.sqlite) storing the simulated output values based on the defined time range and time step (daily, monthly, yearly, or annual average).",
+#             "type": "object",
+#             "contentMediaType": "application/sqlite"
+
 # Output :
 #   id: output_file
-#   Title : River water quantity MITGCM input
+#   Title: River water quantity MITgcm input
 #   Description: txt file (MITGCMinput_RiverWaterQuantityFromSWAT.txt) containing a data frame with the correct formatting and structure to enter MITGCM model. 
 
-#install.packages("readr", "dplyr", "lubridate")
-library(readr)
+#install.packages("DBI","RSQLite", "dplyr")
+library(DBI)
+library(RSQLite)
 library(dplyr)
-library(lubridate)
 
 # INPUTS + example values
 args <- commandArgs(trailingOnly = TRUE)
 print(paste0('R Command line args: ', args))
-file1 <- args[1] # simulation_flo_out.csv
-file2 <- args[2] # simulation_water_temp.csv
-output <- args[3] # /out/MITGCMinput_RiverWaterQuantityFromSWAT.txt
+file1 <- args[1] # thread_1.sqlite
+output <- args[2] # /out/MITGCMinput_RiverWaterQuantityFromSWAT.txt
 
-Conversion_SWAT_MITGCM_WaterQuantity <- function( flow_file, temp_file, output_file){
-  # Read and process water flow data
-  df_flowout <- read_delim(flow_file, delim = ",", show_col_types = FALSE) %>%
-    select(date, flo_out) %>%
-    rename(date = date, flowout = flo_out) %>%
-    mutate(date = ymd(date),  # Use lubridate for better date parsing
-           flowout = as.numeric(flowout))  # Ensure numeric type
+Conversion_SWAT_MITGCM_WaterQuantity <- function( db_path, output_file){
+  # Load packages quietly
+  suppressPackageStartupMessages(lapply(requiredPackages, library, character.only = TRUE))
   
-  # Read and process water temperature data
-  df_temp <- read_delim(temp_file, delim = ",", show_col_types = FALSE) %>%
-    select(date="date", tmp_interpol="water_temp") %>%
-    mutate(date = ymd(date))  # Ensure consistent date format
+  # Create a connection to database
+  con <- dbConnect(RSQLite::SQLite(), dbname = db_path)
   
-  # Merge datasets
-  df <- df_flowout %>%
-    left_join(df_temp, by = "date") %>%
-    mutate(time = "12:00:00",
+  #Connect to DB
+  dbListTables(con)
+  
+  #Read Table
+  my_table <- dbReadTable(con, "run_1_1.1")
+  
+  # Correct date format: 
+  my_table$date <- as.Date(my_table$date, origin = "1970-01-01") #Convert date from, Unix Epoch date format (days since 1970-01-01) to YYYY-MM-DD.
+  
+  df <- my_table %>%
+    mutate(flo_out = format(flo_out, digits = 6, nsmall = 6, scientific = FALSE), #Avoid scientific notation
+           time = "12:00:00",
            date = format(date, "%Y%m%d"),
-           output_data = paste0(date, "-", time, " ", flowout, " ", tmp_interpol))
+           output_data = paste0(date, "-", time, " ", flo_out, " ", water_temp))
+  
+  #Disconnect DB
+  dbDisconnect(con) 
   
   # Export to text file
   write.table(df$output_data, file = output_file, row.names = FALSE, col.names = FALSE, quote = FALSE)
@@ -63,4 +63,4 @@ Conversion_SWAT_MITGCM_WaterQuantity <- function( flow_file, temp_file, output_f
   message("File successfully written: ", output_file)
 }
 
-Conversion_SWAT_MITGCM_WaterQuantity(file1, file2, output)
+Conversion_SWAT_MITGCM_WaterQuantity(file1, output)
