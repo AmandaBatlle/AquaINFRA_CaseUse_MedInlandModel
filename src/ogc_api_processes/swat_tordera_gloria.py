4353f83effc616813933bbca2a4ecdc416043107
Merret Buurman <merret.buurman@igb-berlin.de>
2026-06-23 15:31:12 +0000

    Fixing docker image version to commit 36f61b2.

--- src/ogc_api_processes/swat_tordera_gloria.py
+++ src/ogc_api_processes/swat_tordera_gloria.py
@@ -80,7 +80,7 @@ class TorderaGloriaProcessor(BaseProcessor):
         self.supports_outputs = True
         self.job_id = 'nothing-yet'
         self.process_id = self.metadata["id"]
-        self.image_name = 'catalunya-tordera:20260608-1eccf57'
+        self.image_name = 'catalunya-tordera:20260623-36f61b2'
         self.script_name = 'SWATrunR_AquaINFRAtool_v20260313.R'
         config_file_path = os.environ.get('AQUAINFRA_CONFIG_FILE', "./config.json")
         with open(config_file_path, 'r') as config_file:
